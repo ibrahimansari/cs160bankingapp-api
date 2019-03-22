2d5bb8c7cc37d426a10ad0cf2b60a7118f6ac9a8
samesmaeili <32761208+samesmaeili@users.noreply.github.com>
2019-03-21 23:42:33 -0700

    Update app.js

--- app.js
+++ app.js
@@ -135,7 +135,8 @@ app.post('/api/validateUser', (req, res) => {			//api for validating user when s
 
 			
 			pool.connect(function(err, client, done) {
-				 const query = client.query(new pg.Query("SELECT date, amount, balance from transaction where email=$1 order by date desc", [user.email])))
+				    const query = client.query(new pg.Query("SELECT date, amount, balance from transaction where email=$1 order by date desc", [user.email]))
+
    
 				    query.on('row', (row) => {	//push transaction of user from database to data structure
 					    specificTransaction.push(row);
