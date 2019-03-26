73a170cdbdd2f6c403eccdd593bf8138b237cd1c
samesmaeili <32761208+samesmaeili@users.noreply.github.com>
2019-03-25 23:51:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -261,10 +261,10 @@ app.post('/api/balance', (req, res) => {	//api for getting balance of a customer
 
 	const {email} = req.body
 	
-	var hold = [];		//holds balance
+	const hold = [];		//holds balance
 	
 	pool.connect(function(err, client, done) {
-	    const query = client.query(new pg.Query("SELECT balance from transaction where email=$1 order by date desc LIMIT 1", [email]))
+	    const query = client.query(new pg.Query("SELECT balance from transaction where email=$1 order by date desc", [email]))
 
 	    query.on('row', (row) => {	//push transaction of user from database to data structure
 		    hold.push(row);
