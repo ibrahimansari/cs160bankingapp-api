65238c395e68070d36d3e722a2dfc20f9ae7325a
samesmaeili <32761208+samesmaeili@users.noreply.github.com>
2019-04-11 13:05:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -120,6 +120,7 @@ app.post('/api/validateUser', (req, res) => {			//api for validating user when s
 						console.log(res);
 					    })
 					   query.on("end", function (result) {
+						   console.log(accountArray);
 						res.json({value:val, transactions:specificTransaction, first_name: user.first_name, last_name: user.last_name, email: user.email, address: user.address, zipcode: user.zipcode, accountInfo: accountArray});
 					    });
 
